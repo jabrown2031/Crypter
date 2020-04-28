39f5e8bef38a87c28ccba21a43e9d8c066f842a2
JAB <jabrown@penguin-systems.com>
2020-04-27 22:24:16 -0500

    atbash cipher with UTF16 charspace

--- ciphers/atbash.py
+++ ciphers/atbash.py
@@ -1,3 +1,33 @@
 #Atbash cipher
 #uses UTF-16 character set
 
+def encrypt(text):
+    ctext = "";
+
+    for i in range(len(text)):
+        c = (65535 - ord(text[i]));
+        ctext += hex(c);
+
+    return ctext;
+
+def decrypt(text):
+    ptext = "";
+    
+    for i in range(1, len(text.split('0x'))):
+        c = (65535 - int(text.split('0x')[i], 16));
+        ptext += chr(c);
+
+    return ptext;
+
+def test():
+    text = "TeSt 1 2 3";
+    ctext = encrypt(text);
+    ptext = decrypt(ctext);
+
+    print("Text:", text);
+    print("Cipher Text:", ctext);
+    print("Plain Text:", ptext);
+
+
+test();
+
